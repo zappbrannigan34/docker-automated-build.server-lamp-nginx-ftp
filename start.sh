8f445eb567735f59995a815f82b6f450ef5e3156
Alexey Kuznetsov <kuznetsovalexey34@gmail.com>
2018-01-31 17:27:58 +0300

    add start.sh

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $1 == 'no-bootstrap' ]; then
+  exec bash
+fi
+
+if ( id ${USER} ); then
+  echo "User ${USER} already exists"
+else
+  echo "Creating user ${USER}"
+  ENC_PASS=$(perl -e 'print crypt($ARGV[0], "password")' ${PASS})
+  useradd -d /ftp/${USER} -m -p ${ENC_PASS} -u 1001 -s /bin/sh ${USER}
+fi
+
+if [ $1 == 'vsftpd' ]; then
+  exec vsftpd
+else
+  exec $@
+fi
